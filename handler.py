74fac9a0e4ee5479e70b671d95bf537a846ff6e6
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-06-10 00:50:42 +0900

    Esa Webhookで特定RepositoryをCommitする箇所まで完成

--- handler.py
+++ handler.py
@@ -1,26 +1,38 @@
 import json
+import base64
+import datetime
+import os
 
+from github import Github
+from github import InputGitTreeElement
+
+token = os.environ.get("GITHUB_TOKEN")
+repository = os.environ.get("GITHUB_REPOSITORY")
+branch_name = os.environ.get("BRANCH_NAME")
+commit_dir = os.environ.get("COMMIT_DIR")
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    md_file = event['post']['body_md']
+    md_name = str(datetime.date.today()) + "-" + event['post']['name']
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    # Repository Info
+    repo = Github(token).get_repo(repository)
+    refs = repo.get_git_ref(branch_name)
+    refs_sha = refs.object.sha
 
-    print(response)
+    # Commit file (Create)
+    element = InputGitTreeElement(commit_dir + "/" + md_name, '100644', 'base64', md_file)
 
-    return response
+    # 最新版のCommit情報を取得
+    commit = repo.get_commit(refs_sha).commit
+    base_tree = commit.tree
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+    # 新しくCommitを作成
+    new_tree = repo.create_git_tree([element], base_tree)
+    new_commit = repo.create_git_commit("Create Esa Post by Lambda", new_tree, [commit])
+    refs.edit(new_commit.sha)
+
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+       "statusCode": 200,
+       "body": "Success Git Commit " + new_commit.sha
     }
-    """
