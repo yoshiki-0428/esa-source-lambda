719c6fd1c7a17d8687babdee8b6e1840120a6959
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-06-11 19:51:19 +0900

    [add] esa_signatureを実装

--- handler.py
+++ handler.py
@@ -1,7 +1,8 @@
 import json
-import base64
 import datetime
 import os
+import hmac
+import hashlib
 
 from github import Github
 from github import InputGitTreeElement
@@ -10,81 +11,89 @@ token = os.environ.get("GITHUB_TOKEN")
 repository = os.environ.get("GITHUB_REPOSITORY")
 branch_name = os.environ.get("BRANCH_NAME")
 commit_dir = os.environ.get("COMMIT_DIR")
+esa_secret_key = os.environ.get("ESA_SECRET_KEY").encode('utf-8')
+
 
 def main(event, context):
     esa_data = json.loads(event['body'])
-    print("ESA Payload")
-    print(esa_data)
 
     # Validation
-    if bool(esa_data['post']['wip']):
+    if validate_request(event):
+        print("Unauthorized request")
+        return {
+            "statusCode": 401,
+            "body": "Unauthorized request"
+        }
+    elif bool(esa_data['post']['wip']):
         print("Do not need Commit cause status is wip")
         return {
-            "statusCode": 200,
+            "statusCode": 204,
             "body": "Do not need Commit cause status is wip"
         }
 
+    # find file & file commitment
+    md_text = esa_data['post']['body_md']
     if esa_data['kind'] == "post_create":
         print("post_create")
-        new_commit_file(esa_data)
+        md_name = str(datetime.date.today()) + "--" + str(esa_data['post']['number']) + ".md"
+        commit_file(md_name, md_text)
+
     elif esa_data['kind'] == "post_update":
         print("post_update")
-        update_file(esa_data)
+        commit_file(find_file_name(esa_data["post"]["number"]), md_text)
+
     elif esa_data['kind'] == "post_archive":
         print("post_archive")
         # TODO archive
 
     return {
-       "statusCode": 200,
-       "body": "Success Git Commit"
+        "statusCode": 200,
+        "body": "Success Git Commit"
     }
 
-def new_commit_file(esa_data):
-    md_file = esa_data['post']['body_md']
-    md_name = str(datetime.date.today()) + "--" + str(esa_data['post']['number']) + ".md"
 
-    # Repository Info
+# Repository Info
+def get_repo():
     repo = Github(token).get_repo(repository)
     refs = repo.get_git_ref(branch_name)
-    refs_sha = refs.object.sha
+    return {
+        "repo": repo,
+        "refs": refs
+    }
+
+
+def commit_file(commit_name, commit_file_text):
+    if commit_name == "" or commit_file_text == "":
+        return
+    # Repository Info
+    repo_info = get_repo()
 
     # Commit file (Create)
-    element = InputGitTreeElement(commit_dir + "/" + md_name, '100644', 'base64', md_file)
+    element = InputGitTreeElement(commit_dir + "/" + commit_name, '100644', 'base64', commit_file_text)
 
     # 最新版のCommit情報を取得
-    commit = repo.get_commit(refs_sha).commit
+    commit = repo_info['repo'].get_commit(repo_info['refs'].object.sha).commit
     base_tree = commit.tree
 
     # 新しくCommitを作成
-    new_tree = repo.create_git_tree([element], base_tree)
-    new_commit = repo.create_git_commit("Create Esa Post by Lambda", new_tree, [commit])
-    refs.edit(new_commit.sha)
+    new_tree = repo_info['repo'].create_git_tree([element], base_tree)
+    new_commit = repo_info['repo'].create_git_commit("Create Esa Post by Lambda: " + commit_name, new_tree, [commit])
+    repo_info['refs'].edit(new_commit.sha)
 
-def update_file(esa_data):
-    md_file = esa_data['post']['body_md']
 
-    # Repository Info
-    repo = Github(token).get_repo(repository)
-    refs = repo.get_git_ref(branch_name)
-    refs_sha = refs.object.sha
-
-    # Commit file (Create)
-    element = InputGitTreeElement(commit_dir + "/" + find_file_name(repo, esa_data["post"]["number"]), '100644', 'base64', md_file)
-
-    # 最新版のCommit情報を取得
-    commit = repo.get_commit(refs_sha).commit
-    base_tree = commit.tree
-
-    # 新しくCommitを作成
-    new_tree = repo.create_git_tree([element], base_tree)
-    new_commit = repo.create_git_commit("Create Esa Post by Lambda", new_tree, [commit])
-    refs.edit(new_commit.sha)
-
-def find_file_name(repo, esa_number):
-    for content in repo.get_contents(commit_dir + "/"):
+def find_file_name(esa_number):
+    for content in get_repo()['repo'].get_contents(commit_dir + "/"):
         content_name = content.name
         split_content_name = content_name.split("--")
         if str(esa_number) in split_content_name[len(split_content_name) - 1]:
             return content_name
     print("File is None")
     return ""
+
+
+# signature check
+def validate_request(event):
+    payload = event['body']
+    size = len(event['headers']['X-Esa-Signature'].split("sha256="))
+    esa_signature = event['headers']['X-Esa-Signature'].split("sha256=")[size - 1]
+    return not hmac.new(esa_secret_key, payload.encode('utf-8'), hashlib.sha256).hexdigest() == esa_signature
